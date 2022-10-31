bf3fe0ae5fb50d12055bd26d818c2dc1801749a8
Andrew Aramouni <aramouni8@gmail.com>
2022-10-31 14:22:40 -0400

    add react

---
+++ shopping-cart/src/ProductsList/ProductsList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+//import { Test } from './ProductsList.styles';
+
+const ProductsList = (props) => <div className="ProductsListWrapper"></div>;
+
+ProductsList.propTypes = {
+  // bla: PropTypes.string,
+};
+
+ProductsList.defaultProps = {
+  // bla: 'test',
+};
+
+export default ProductsList;
